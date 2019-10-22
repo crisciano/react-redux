29523f9618be75b126d994befb0f7995ad130f54
crisciano <criscianosb2@live.com>
2019-10-22 13:28:13 -0300

    Implement hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [ name, setName ] = useState('');
+  const [ items, setItems ] = useState([])
+  const [ title ] = useState('React redux')
+
+  useEffect( () => {
+    document.title = title
+  })
+
+  function handleChangeName(e){
+    setName(e.target.value)
+  }
+
+  function addItem(item){
+    setItems([ ...items, { id: items.length + 1, name: item }])
+    setName('')
+  }
+  function removeItem(id){
+    console.log(id);
+    setItems(items.filter( item => item.id !== id ))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <label>Name</label>
+        <input 
+          value={name}  
+          onChange={handleChangeName} 
+        /> 
+        {/* <p> {name} </p> */}
+        <button onClick={() => addItem(name)} > Salve </button>
+        <ul>
+          {
+            items.map( item => (
+              <li key={item.id} >{item.name} <button onClick={() => removeItem(item.id) } > remove </button> </li>
+            ))
+          }
+
+        </ul>
       </header>
     </div>
   );
